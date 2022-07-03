5069f83cafe9f431364b76297047d4f846feaa97
Mallika Sharda <67362501+mallikasharda12@users.noreply.github.com>
2022-07-04 00:02:28 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Videos",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin{
+
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.green));
+    return Scaffold(
+      appBar: AppBar(
+        title: TabBar(
+          indicator: UnderlineTabIndicator(
+            insets: EdgeInsets.symmetric(horizontal: 16.0)
+          ),
+          controller: controller,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.home)),
+            Tab(icon: Icon(Icons.person_add)),
+            Tab(icon: Icon(Icons.notifications)),
+            Tab(icon: Icon(Icons.clear_all)),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: <Widget>[
+          HomePage(),
+          HomePage(),
+          HomePage(),
+          HomePage(),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List name = ["Mallika", "Khushi", "Tanya", "Shagun", "Dhawal", "Abhiram", "Daksh", "Utkarsh", "Aditya", "Dushyant"];
+  List desg = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ", "XYZ"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.green[100],
+        child: ListView.builder(
+          itemCount: 10,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            child: Card(
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0)
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: 55.0,
+                          height: 55.0,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.green,
+                            backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUuX2qMyuelySk6FJr7uWpSmWNRiVLcDflNg&usqp=CAU"),
+                          ),
+                        ),
+                        SizedBox(width: 6.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(name[index], style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold)),
+                            SizedBox(height: 5.0),
+                            Text(desg[index], style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                      child: FlatButton(
+                        onPressed: () {},
+                        color: Colors.red[200],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Text("Invite", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
